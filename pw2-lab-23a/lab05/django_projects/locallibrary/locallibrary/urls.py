39887627b94ebbb111b99ab2b9b9af86ff46dabc
AndreRH09 <aanazcoh@unsa.edu.pe>
2023-06-20 11:00:21 -0500

    paso 8 completado

--- pw2-lab-23a/lab05/django_projects/locallibrary/locallibrary/urls.py
+++ pw2-lab-23a/lab05/django_projects/locallibrary/locallibrary/urls.py
@@ -30,5 +30,10 @@ urlpatterns += [
 urlpatterns += [
     path('', RedirectView.as_view(url='catalog/', permanent=True)),
 ]
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+# Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
